f69f22b10464a4a9707e2e0fffb1c0e9384693b7
Rodrigo Hernández <2823897250101@ingenieria.usac.edu.gt>
2023-04-28 07:48:21 -0600

    Agregando logica del login del frontend

--- Frontend/miap2_frontend/src/Services/APIs/Parser.js
+++ Frontend/miap2_frontend/src/Services/APIs/Parser.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create(
     {
         baseURL: 'http://localhost:3000/api',
-        timeout: 180000,
+        timeout: 600000,
         headers: {
             'Content-Type': 'application/json',
         }
@@ -13,5 +13,11 @@ const instance = axios.create(
 export const parse = async (value) => {
     console.log(value);
     const { data } = await instance.post('/consola', { comando: value });
+    return data;
+}
+
+export const login = async (usuario, id, password) => {
+    console.log(usuario, id, password);
+    const { data } = await instance.post('/login', { id_particion: id, usuario: usuario, password: password });
     return data;
 }
--- Frontend/miap2_frontend/src/pages/Login.jsx
+++ Frontend/miap2_frontend/src/pages/Login.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import './login.css';
 import { Link } from 'react-router-dom';
+import Service from "../Services/Service";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [loguear, setLoguear] = useStare({
+        id_particion: '',
+        usuario: '',
+        password: ''
+    })
+    const id_text = useRef(null);
+    const user_text = useRef(null);
+    const password_text = useRef(null);
+
+    const handleChange = e => {
+        setLoguear({
+            ...loguear,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handlerLogin = () => {
+        Service.login(loguear.usuario,loguear.id_particion,loguear.password)
+        .then(({respuesta}) => {
+            if (respuesta){
+                alert("Bienvenido " + loguear.usuario)
+                Navigate('/reportes')
+            }else{
+                alert("Lo lamento, no puedes iniciar sesión, verifica tus credenciales")
+            }
+        })
+    }
+
     return (
         <>
             <div className="body_div">
@@ -12,24 +43,48 @@ const Login = () => {
                     <form action="" className="log-in" autocomplete="off"> 
                     <h4><img src="src/assets/images/technology.png" alt="" /> <span>Login</span></h4>
                     <div className="floating-label">
-                        <input placeholder="ID Partición" type="text" name="id_particion" id="id_particion" autocomplete="off"/>
+                        <input 
+                            placeholder="ID Partición" 
+                            type="text" 
+                            name="id_particion" 
+                            id="id_particion" 
+                            autocomplete="off" 
+                            value={loguear.id_particion}
+                            onChange={handleChange}
+                        />
                         <label for="id_particion">ID Partición:</label>
                         <div className="icon">
                         </div>
                     </div>
                     <div className="floating-label">
-                        <input placeholder="User" type="text" name="user" id="user" autocomplete="off"/>
+                        <input 
+                            placeholder="User" 
+                            type="text" 
+                            name="usuario" 
+                            id="user" 
+                            autocomplete="off"
+                            value={loguear.usuario}
+                            onChange={handleChange}
+                        />
                         <label for="user">User:</label>
                         <div className="icon">
                         </div>
                     </div>
                     <div className="floating-label">
-                        <input placeholder="Password" type="password" name="password" id="password" autocomplete="off"/>
+                        <input 
+                            placeholder="Password" 
+                            type="password" 
+                            name="password" 
+                            id="password" 
+                            autocomplete="off"
+                            value={loguear.password}
+                            onChange={handleChange}
+                        />
                         <label for="password">Password:</label>
                         <div className="icon">
                         </div>                        
                     </div>
-                    <button type="submit" onClick="return false;">Log in</button>
+                    <button type="submit" onClick={handlerLogin}>Log in</button>
                     <Link to="/" className="discrete">Regresar</Link>
                     </form>
                 </div>
